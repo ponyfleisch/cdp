c3a798c165334a6f0cb524169e44ea59c64f019d
Claudio Mettler <github@ponyfleisch.ch>
2016-11-24 12:37:13 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# cdp protocol implementation
+
+See http://www.ciholas.com/downloads/dwusb/CDP-2016.08.15.pdf
+
+Currently only emits `position` events (`0x100`)
+
+## usage
+
+```javascript
+const cdp = require('cdp');
+
+// optional arguments:
+// address: ip address to listen on (default: all)
+// mcast: multicast address to subscribe to (default: 239.255.76.67)
+const server = cdp.start();
+
+server.on('position', (data) => {
+    console.log(data);
+});
+```
+
+data format:
+
+```json
+{
+  "tag": "0x10006c0",
+  "position": {
+    "x": 9.415,
+    "y": 1.306,
+    "z": 1.627
+  }
+}
+```
+
+## server setup
+
+Make sure the "external" protocol stream goes to the network you're listening on.
---
+++ index.js
@@ -0,0 +1,108 @@
+'use strict';
+const dgram = require('dgram');
+const EventEmitter = require('events');
+
+const emitter = new EventEmitter();
+
+var server;
+
+function start(address, mcast){
+    stop();
+
+    server = dgram.createSocket('udp4');
+
+    server.on('error', (err) => {
+        server.close();
+    });
+
+    server.on('message', (msg, rinfo) => {
+        parseMessage(msg);
+    });
+
+    server.on('listening', () => {
+        if(mcast){
+            server.addMembership(mcast);
+        }else{
+            server.addMembership('239.255.76.67');
+        }
+    });
+
+    server.bind(7667, address);
+
+    return emitter;
+}
+
+function stop(){
+    if(server){
+        server.stop();
+        server = false;
+    }
+}
+
+function parseMessage(msg){
+    if(msg.readUInt32LE() == 0x3230434c && msg.length >= 24){
+        let header = {
+            seq: msg.readUInt32LE(4),
+            proto: msg.toString('utf8', 8, 15),
+            device: '0x'+msg.readUInt32LE(16).toString(16),
+            packetSize: msg.length
+        };
+
+        let pos = 20;
+        while(pos < msg.length){
+            let dataItem = parseDataItem(msg.slice(pos));
+            if(dataItem.header.type == '0x100'){
+                emitter.emit('position', {
+                    tag: header.device,
+                    position: dataItem.data.coordinates
+                });
+            }
+            pos += dataItem.header.size + 4;
+        }
+    }
+}
+
+function parseDataItem(buf){
+    let item = {
+        type: '0x'+buf.readUInt16LE(0).toString(16),
+        size: buf.readUInt16LE(2),
+    };
+    let packet;
+    switch(item.type){
+        case '0x103':
+            // anchor status:
+            packet = {
+                serial: '0x'+buf.readUInt32LE(4).toString(16),
+                status: buf[8]
+            };
+            break;
+        case '0x102':
+            // infrastructure position
+            packet = {
+                nodeType: buf[4]==1?'master':'anchor',
+                serial: '0x'+buf.readUInt32LE(5).toString(16),
+                coordinates: parseCoordinates(buf.slice(9, 21))
+            };
+            break;
+        case '0x100':
+            // position
+            packet = {
+                coordinates: parseCoordinates(buf.slice(4, 16)),
+            };
+    }
+
+    return {header: item, data: packet};
+}
+
+function parseCoordinates(buf){
+    return {
+        x: buf.readInt32LE(0)/1000,
+        y: buf.readInt32LE(4)/1000,
+        z: buf.readInt32LE(8)/1000
+    };
+}
+
+module.exports = {
+    start: start,
+    stop: stop
+};
